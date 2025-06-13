d5bc69a50bcf3de6a0f2c51b155cea4de43c80f7
zeeshan <muhammad.zeeshan@brainxtech.com>
2025-06-13 20:16:04 +0500

    path change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/video-analysis/',
+  base: '/video-analysis',
   plugins: [react()],
 })
